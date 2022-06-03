88e0779d2ed04a12eac99b46da6a932dc1eec10f
tusharbilasakar <tusharbilasakar@gmail.com>
2022-06-03 12:54:03 +0530

    assignment1

---
+++ src/util/helper.js
@@ -0,0 +1,16 @@
+const printDate = function(){
+    console.log("2-05-2022")
+}
+const printMonth = function () {
+    console.log ("june")
+
+}
+
+const printBatchinfo = function(){
+    console.log ("Radon W3D3 the topic for today is Nodejs module system")
+	
+
+}
+module.exports.printdate=printDate
+module.exports.printMonth=printMonth
+module.exports.printbatchinfo=printBatchinfo
